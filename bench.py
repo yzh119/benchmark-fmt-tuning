f4236692abb1f6720b12a278bdbe5d391df04b8a
Ubuntu <ubuntu@ip-172-31-26-2.us-east-2.compute.internal>
2020-06-03 09:34:08 +0000

    upd

--- bench.py
+++ bench.py
@@ -61,7 +61,7 @@ def get_multi_head_sddmm_flops(g, num_hid, num_heads):
         with th_op_time() as timer:
             g.apply_edges2(fn.u_dot_v('h', 'x', 'h'))
 
-        if t >= 10:
+        if t >= 30:
             accum_time += timer.time 
             accum_FLOPs += g.number_of_edges() * num_heads * num_hid
     g.ndata.clear()
@@ -77,7 +77,7 @@ def get_sddmm_flops(g, num_hid):
         with th_op_time() as timer:
             g.apply_edges2(fn.u_dot_v('h', 'x', 'h'))
 
-        if t >= 10:
+        if t >= 30:
             accum_time += timer.time 
             accum_FLOPs += g.number_of_edges() * num_hid
     g.ndata.clear()
@@ -85,52 +85,60 @@ def get_sddmm_flops(g, num_hid):
     return accum_FLOPs / accum_time
 
 def benchmark(g, dataset):
-    f = open(dataset + '.csv', 'w')
-    print(g)
+    f1 = open(dataset + '_spmm.csv', 'w')
+    f2 = open(dataset + '_sddmm.csv', 'w')
+    print(g, dataset)
     num_heads = 8
-    with f:
-        writer = csv.writer(f)
-        writer.writerow(['feat size', 'coo 8-head', 'coo', 'csr 8-head', 'csr'])
+    with f1, f2:
+        spmm_writer = csv.writer(f1)
+        spmm_writer.writerow(['feat size', 'coo 8-head', 'coo', 'csr 8-head', 'csr'])
+        sddmm_writer = csv.writer(f2)
+        sddmm_writer.writerow(['feat size', 'coo 8-head', 'coo', 'csr 8-head', 'csr'])
 
         for num_hid in [1, 2, 4, 8, 16, 32, 64, 128]:
-            row = []
+            row_spmm = []
+            row_sddmm = []
             print('feat size: {}'.format(num_hid))
-            row.append(num_hid)
+            row_spmm.append(num_hid)
+            row_sddmm.append(num_hid)
             # COO format speed.
             dgl.spfmt.set_format('coo')
             multi_head_spmm = get_multi_head_spmm_flops(g, num_hid, num_heads)
-            #multi_head_sddmm_time = get_multi_head_sddmm_flops(g, num_hid, num_heads)
+            multi_head_sddmm = get_multi_head_sddmm_flops(g, num_hid, num_heads)
             spmm = get_spmm_flops(g, num_hid)
-            #sddmm_time = get_sddmm_flops(g, num_hid)
+            sddmm = get_sddmm_flops(g, num_hid)
             print('multi-head spmm FLOPS : ', multi_head_spmm)
-            #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
+            print('multi-head sddmm FLOPS : ', multi_head_sddmm)
             print('spmm FLOPS : ', spmm)
-            #print('sddmm FLOPS : ', sddmm_time)
-            row.append(multi_head_spmm / 1e9)
-            row.append(spmm / 1e9)
+            print('sddmm FLOPS : ', sddmm)
+            row_spmm.append(multi_head_spmm / 1e9)
+            row_spmm.append(spmm / 1e9)
+            row_sddmm.append(multi_head_sddmm / 1e9)
+            row_sddmm.append(sddmm / 1e9)
             
             # CSR format speed.
             dgl.spfmt.set_format('csr')
             multi_head_spmm = get_multi_head_spmm_flops(g, num_hid, num_heads)
-            #multi_head_sddmm_time = get_multi_head_sddmm_flops(g, num_hid, num_heads)
+            multi_head_sddmm = get_multi_head_sddmm_flops(g, num_hid, num_heads)
             spmm = get_spmm_flops(g, num_hid)
-            #sddmm_time = get_sddmm_flops(g, num_hid)
+            sddmm = get_sddmm_flops(g, num_hid)
             print('multi-head spmm FLOPS : ', multi_head_spmm)
-            #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
+            print('multi-head sddmm FLOPS : ', multi_head_sddmm)
             print('spmm FLOPS : ', spmm)
-            #print('sddmm FLOPS : ', sddmm_time)
+            print('sddmm FLOPS : ', sddmm)
 
-            row.append(multi_head_spmm / 1e9)
-            row.append(spmm / 1e9)
-            writer.writerow(row)
+            row_spmm.append(multi_head_spmm / 1e9)
+            row_spmm.append(spmm / 1e9)
+            row_sddmm.append(multi_head_sddmm / 1e9)
+            row_sddmm.append(sddmm / 1e9)
+            spmm_writer.writerow(row_spmm)
+            sddmm_writer.writerow(row_sddmm)
 
 
 if __name__ == '__main__':
-    """
     Dataset = namedtuple('Dataset', ['dataset'])
     data = load_data(Dataset(dataset='reddit'))
     benchmark(dgl.graph(data.graph.edges()), 'reddit')
-    """
     """
     for K in [8, 16, 32, 64]:
         filename = 'modelnet40_{}.g'.format(K)
@@ -139,6 +147,8 @@ if __name__ == '__main__':
         g = dgl.graph(dgl.batch(data).edges())
         benchmark(g, 'mesh_{}'.format(K))
     """
+    """
     ppi = PPIDataset('train')
     benchmark(dgl.graph(ppi.graph.edges()), 'ppi')
+    """
     

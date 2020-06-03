0e921dbd8d26248d4c4485bddae174149063a76f
Ubuntu <ubuntu@ip-172-31-26-2.us-east-2.compute.internal>
2020-06-03 06:22:05 +0000

    upd

--- bench.py
+++ bench.py
@@ -3,10 +3,11 @@ import time
 import dgl.function as fn
 import torch as th
 import pickle
+import csv
 from dgl.data import load_data, PPIDataset
 from collections import namedtuple
 
-def get_multi_head_spmm_time(g, num_hid, num_heads):
+def get_multi_head_spmm_flops(g, num_hid, num_heads):
     g.ndata['h'] = th.rand(g.number_of_nodes(), num_heads, num_hid).cuda()
     g.edata['w'] = th.rand(g.number_of_edges(), num_heads, 1).cuda()
     accum_time = 0
@@ -26,7 +27,7 @@ def get_multi_head_spmm_time(g, num_hid, num_heads):
     g.edata.clear()
     return accum_FLOPs / accum_time
 
-def get_spmm_time(g, num_hid):
+def get_spmm_flops(g, num_hid):
     g.ndata['h'] = th.rand(g.number_of_nodes(), num_hid).cuda()
     g.edata['w'] = th.rand(g.number_of_edges(), 1).cuda()
     accum_time = 0
@@ -46,7 +47,7 @@ def get_spmm_time(g, num_hid):
     g.edata.clear()
     return accum_FLOPs / accum_time
 
-def get_multi_head_sddmm_time(g, num_hid, num_heads):
+def get_multi_head_sddmm_flops(g, num_hid, num_heads):
     g.ndata['h'] = th.rand(g.number_of_nodes(), num_heads, num_hid).cuda()
     g.ndata['x'] = th.rand(g.number_of_nodes(), num_heads, num_hid).cuda()
     accum_time = 0
@@ -66,7 +67,7 @@ def get_multi_head_sddmm_time(g, num_hid, num_heads):
     g.edata.clear()
     return accum_FLOPs / accum_time
 
-def get_sddmm_time(g, num_hid):
+def get_sddmm_flops(g, num_hid):
     g.ndata['h'] = th.rand(g.number_of_nodes(), num_hid).cuda()
     g.ndata['x'] = th.rand(g.number_of_nodes(), num_hid).cuda()
     accum_time = 0
@@ -86,50 +87,62 @@ def get_sddmm_time(g, num_hid):
     g.edata.clear()
     return accum_FLOPs / accum_time
 
-def benchmark(g):
+def benchmark(g, dataset):
+    f = open(dataset + '.csv', 'w')
     print(g)
     num_heads = 8
-    for num_hid in [1, 2, 4, 8, 16, 32, 64, 128]:
-        print('feat size: {}'.format(num_hid))
-        # COO format speed.
-        dgl.spfmt.set_format('coo')
-        multi_head_spmm_time = get_multi_head_spmm_time(g, num_hid, num_heads)
-        #multi_head_sddmm_time = get_multi_head_sddmm_time(g, num_hid, num_heads)
-        spmm_time = get_spmm_time(g, num_hid)
-        #sddmm_time = get_sddmm_time(g, num_hid)
-        print('multi-head spmm FLOPS : ', multi_head_spmm_time)
-        #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
-        print('spmm FLOPS : ', spmm_time)
-        #print('sddmm FLOPS : ', sddmm_time)
-        
-        # CSR format speed.
-        dgl.spfmt.set_format('csr')
-        multi_head_spmm_time = get_multi_head_spmm_time(g, num_hid, num_heads)
-        #multi_head_sddmm_time = get_multi_head_sddmm_time(g, num_hid, num_heads)
-        spmm_time = get_spmm_time(g, num_hid)
-        #sddmm_time = get_sddmm_time(g, num_hid)
-        print('multi-head spmm FLOPS : ', multi_head_spmm_time)
-        #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
-        print('spmm FLOPS : ', spmm_time)
-        #print('sddmm FLOPS : ', sddmm_time)
- 
+    with f:
+        writer = csv.writer(f)
+        writer.writerow(['feat size', 'coo 8-head', 'coo', 'csr 8-head', 'csr'])
+
+        for num_hid in [1, 2, 4, 8, 16, 32, 64, 128]:
+            row = []
+            print('feat size: {}'.format(num_hid))
+            row.append(num_hid)
+            # COO format speed.
+            dgl.spfmt.set_format('coo')
+            multi_head_spmm = get_multi_head_spmm_flops(g, num_hid, num_heads)
+            #multi_head_sddmm_time = get_multi_head_sddmm_flops(g, num_hid, num_heads)
+            spmm = get_spmm_flops(g, num_hid)
+            #sddmm_time = get_sddmm_flops(g, num_hid)
+            print('multi-head spmm FLOPS : ', multi_head_spmm)
+            #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
+            print('spmm FLOPS : ', spmm)
+            #print('sddmm FLOPS : ', sddmm_time)
+            row.append(multi_head_spmm / 1e9)
+            row.append(spmm / 1e9)
+            
+            # CSR format speed.
+            dgl.spfmt.set_format('csr')
+            multi_head_spmm = get_multi_head_spmm_flops(g, num_hid, num_heads)
+            #multi_head_sddmm_time = get_multi_head_sddmm_flops(g, num_hid, num_heads)
+            spmm = get_spmm_flops(g, num_hid)
+            #sddmm_time = get_sddmm_flops(g, num_hid)
+            print('multi-head spmm FLOPS : ', multi_head_spmm)
+            #print('multi-head sddmm FLOPS : ', multi_head_sddmm_time)
+            print('spmm FLOPS : ', spmm)
+            #print('sddmm FLOPS : ', sddmm_time)
+
+            row.append(multi_head_spmm / 1e9)
+            row.append(spmm / 1e9)
+            writer.writerow(row)
+
 
 if __name__ == '__main__':
-    #g = dgl.rand_graph(1000, 10000)
     Dataset = namedtuple('Dataset', ['dataset'])
     data = load_data(Dataset(dataset='reddit'))
-    benchmark(dgl.graph(data.graph.edges()))
+    benchmark(dgl.graph(data.graph.edges()), 'reddit')
     """
     for K in [8, 16, 32, 64]:
         filename = 'modelnet40_{}.g'.format(K)
         with open(filename, 'rb') as f:
             data = pickle.load(f)
         g = dgl.graph(dgl.batch(data).edges())
-        benchmark(g)
+        benchmark(g, 'mesh_{}'.format(K))
     """
     """
     ppi = PPIDataset('train')
-    benchmark(dgl.graph(ppi.graph.edges()))
+    benchmark(dgl.graph(ppi.graph.edges()), 'ppi')
     """
 
     
